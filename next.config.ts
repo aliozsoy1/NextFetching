77caef41d78754ac52a7e95d9f7eb7a262e6269c
aliozsoy1 <aliozsoy237@gmail.com>
2024-12-22 23:15:47 +0300

    ..

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === 'production';
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    unoptimized: true, // Disable default image optimization
+  },
+  assetPrefix: isProd ? '/NextFetching' : '',
+  basePath: isProd ? '/NextFetching' : '',
+  output: 'export'
 };
 
 export default nextConfig;
